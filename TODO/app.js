6e04e238b7d47738b2d12c8ac1c0dc11e4f1edeb
mfvvfm <moisesveiga@hotmail.co.uk>
2021-08-24 16:16:42 +0100

    adding filter function to todo list

--- TODO/app.js
+++ TODO/app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 // This grabs '.todo-button' class and runs addTodo function
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 //Functions
 
@@ -42,8 +44,11 @@ function deleteCheck(e) {
     // DELETE TODO
     if (item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
+        // ANIMATION
         todo.classList.add("fall");
-        todo.remove();
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        });
     }
 
     // CHECK MARK
@@ -52,4 +57,32 @@ function deleteCheck(e) {
         todo.classList.toggle("completed");
     }
 
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+      
+    todos.forEach(function(todo) {
+        // This brings back value of whatever option selected
+       switch (e.target.value) {
+           case "all":
+              todo.style.display = "flex";
+              break;
+            case "completed":
+              if (todo.classList.contains("completed")) {
+                  todo.style.display = "flex"; 
+                } else {
+                    todo.style.display = "none";
+                }
+              break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+              break;
+       }
+    });
 }
