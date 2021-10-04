50ad643106af305968027d0aaa48852f2398f316
mfvvfm <moisesveiga@hotmail.co.uk>
2021-10-04 17:46:55 +0100

    initial commit

---
+++ typescript-todo/src/App.tsx
@@ -0,0 +1,34 @@
+import React, {  useState } from 'react';
+import './App.css';
+import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  const addTodo: AddTodo = newTodo => {
+    if (newTodo !== "") {
+      setTodos([...todos, {text: newTodo, complete: false}]);
+      }
+    };
+
+    const toggleComplete: ToggleComplete = selectedTodo => {
+      const updatedTodos = todos.map(todo => {
+        if (todo === selectedTodo) {
+          return {...todo, complete: !todo.complete};
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+    };
+  
+    return (
+      <div className="todo-app">
+          <h1>Todo App</h1>
+        <TodoForm addTodo={addTodo}/>
+        <TodoList todos={todos} toggleComplete={toggleComplete}/>
+      </div>
+    );
+  };
+  
+  export default App;
